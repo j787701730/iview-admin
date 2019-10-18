e0b205df83200729710ae831aa74c7b21bc87b4f
zhengw <247276359@qq.com>
2019-10-18 17:32:37 +0800

    整理代码

--- src/view/article/class.vue
+++ src/view/article/class.vue
@@ -1,12 +1,226 @@
 <template>
-<div>
-  <h1>文章栏目</h1>
-</div>
+  <div>
+    <h1>文章栏目</h1>
+    <div style="height: 10px"></div>
+    <Table :columns="columns10" :data="data9" border></Table>
+    <div style="height: 15px"></div>
+    <Table :columns="columns" :data="data">
+      <template slot-scope="{ row, index }" slot="name">
+        <Input type="text" @click.native.stop="" v-model="editName" v-if="editIndex === index"/>
+        <span v-else>{{ row.name }}</span>
+      </template>
+
+      <template slot-scope="{ row, index }" slot="age">
+        <Input type="text" @click.native.stop="" v-model="editAge" v-if="editIndex === index"/>
+        <span v-else>{{ row.age }}</span>
+      </template>
+
+      <template slot-scope="{ row, index }" slot="birthday">
+        <Input type="text" @click.native.stop="" v-model="editBirthday" v-if="editIndex === index"/>
+        <span v-else>{{ getBirthday(row.birthday) }}</span>
+      </template>
+
+      <template slot-scope="{ row, index }" slot="address">
+        <Input type="text" @click.native.stop="" v-model="editAddress" v-if="editIndex === index"/>
+        <span v-else>{{ row.address }}</span>
+      </template>
+
+      <template slot-scope="{ row, index }" slot="action">
+        <div v-if="editIndex === index">
+          <Button @click="handleSave(index)">保存</Button>
+          &nbsp;<Button @click="editIndex = -1">取消</Button>
+        </div>
+        <div v-else>
+          <Button @click.stop="handleEdit(row, index)">操作</Button>
+        </div>
+      </template>
+    </Table>
+    <div style="height: 15px;"></div>
+  </div>
 </template>
 
 <script>
+import expandRow from './table-expand.vue'
+import { on, off } from '@/libs/tools'
+
 export default {
-  name: 'xxxxx'
+  name: 'article-class',
+  data () {
+    return {
+      columns10: [
+        {
+          type: 'expand',
+          width: 50,
+          render: (h, params) => {
+            return h(expandRow, {
+              props: {
+                row: params.row
+              }
+            })
+          }
+        },
+        {
+          title: 'Name',
+          key: 'name',
+          width: 100,
+          resizable: true
+        },
+        {
+          title: 'Age',
+          key: 'age',
+          width: 200,
+          resizable: true
+        },
+        {
+          title: 'Address',
+          key: 'address'
+        }
+      ],
+      data9: [
+        {
+          name: 'John Brown',
+          age: 18,
+          address: 'New York No. 1 Lake Park',
+          job: 'Data engineer',
+          interest: 'badminton',
+          birthday: '1991-05-14',
+          book: 'Steve Jobs',
+          movie: 'The Prestige',
+          music: 'I Cry',
+          children: [
+            {
+              name: 'John Brown',
+              age: 18,
+              address: 'New York No. 1 Lake Park',
+              job: 'Data engineer',
+              interest: 'badminton',
+              birthday: '1991-05-14',
+              book: 'Steve Jobs',
+              movie: 'The Prestige',
+              music: 'I Cry'
+            },
+            {
+              name: 'John Brown',
+              age: 18,
+              address: 'New York No. 1 Lake Park',
+              job: 'Data engineer',
+              interest: 'badminton',
+              birthday: '1991-05-14',
+              book: 'Steve Jobs',
+              movie: 'The Prestige',
+              music: 'I Cry'
+            }
+          ]
+        },
+        {
+          name: 'Jim Green',
+          age: 25,
+          address: 'London No. 1 Lake Park',
+          job: 'Data Scientist',
+          interest: 'volleyball',
+          birthday: '1989-03-18',
+          book: 'My Struggle',
+          movie: 'Roman Holiday',
+          music: 'My Heart Will Go On'
+        },
+        {
+          name: 'Joe Black',
+          age: 30,
+          address: 'Sydney No. 1 Lake Park',
+          job: 'Data Product Manager',
+          interest: 'tennis',
+          birthday: '1992-01-31',
+          book: 'Win',
+          movie: 'Jobs',
+          music: 'Don’t Cry'
+        },
+        {
+          name: 'Jon Snow',
+          age: 26,
+          address: 'Ottawa No. 2 Lake Park',
+          job: 'Data Analyst',
+          interest: 'snooker',
+          birthday: '1988-7-25',
+          book: 'A Dream in Red Mansions',
+          movie: 'A Chinese Ghost Story',
+          music: 'actor'
+        }
+      ],
+      columns: [
+        {
+          title: '姓名',
+          slot: 'name'
+        },
+        {
+          title: '年龄',
+          slot: 'age'
+        },
+        {
+          title: '出生日期',
+          slot: 'birthday'
+        },
+        {
+          title: '地址',
+          slot: 'address'
+        },
+        {
+          title: '操作',
+          slot: 'action'
+        }
+      ],
+      data: [
+        {
+          name: '王小明',
+          age: 18,
+          birthday: '919526400000',
+          address: '北京市朝阳区芍药居'
+        },
+        {
+          name: '张小刚',
+          age: 25,
+          birthday: '696096000000',
+          address: '北京市海淀区西二旗'
+        }
+      ],
+      editIndex: -1, // 当前聚焦的输入框的行数
+      editName: '', // 第一列输入框，当然聚焦的输入框的输入内容，与 data 分离避免重构的闪烁
+      editAge: '', // 第二列输入框
+      editBirthday: '', // 第三列输入框
+      editAddress: '' // 第四列输入框
+    }
+  },
+  methods: {
+    handleEdit (row, index) {
+      this.editName = row.name
+      this.editAge = row.age
+      this.editAddress = row.address
+      this.editBirthday = row.birthday
+      this.editIndex = index
+    },
+    handleSave (index) {
+      this.data[index].name = this.editName
+      this.data[index].age = this.editAge
+      this.data[index].birthday = this.editBirthday
+      this.data[index].address = this.editAddress
+      this.editIndex = -1
+    },
+    getBirthday (birthday) {
+      const date = new Date(parseInt(birthday))
+      const year = date.getFullYear()
+      const month = date.getMonth() + 1
+      const day = date.getDate()
+      return `${year}-${month}-${day}`
+    },
+    docClick: function () {
+      this.editIndex = -1
+    }
+  },
+  mounted () {
+    on(document, 'click', this.docClick)
+  },
+  beforeDestroy () {
+    off(document, 'click', this.docClick)
+  }
 }
 </script>
 
--- src/view/article/list.vue
+++ src/view/article/list.vue
@@ -5,42 +5,8 @@
 <template>
   <div>
     <h1 style="margin-bottom: 10px;">文章列表</h1>
-    <Button @click="getData">请求</Button>
-    <div style="height: 15px"></div>
-    <Table :columns="columns10" :data="data9" border></Table>
-    <div style="height: 15px"></div>
-    <Table :columns="columns" :data="data">
-      <template slot-scope="{ row, index }" slot="name">
-        <Input type="text" @click.native.stop="" v-model="editName" v-if="editIndex === index"/>
-        <span v-else>{{ row.name }}</span>
-      </template>
-
-      <template slot-scope="{ row, index }" slot="age">
-        <Input type="text" @click.native.stop="" v-model="editAge" v-if="editIndex === index"/>
-        <span v-else>{{ row.age }}</span>
-      </template>
-
-      <template slot-scope="{ row, index }" slot="birthday">
-        <Input type="text" @click.native.stop="" v-model="editBirthday" v-if="editIndex === index"/>
-        <span v-else>{{ getBirthday(row.birthday) }}</span>
-      </template>
-
-      <template slot-scope="{ row, index }" slot="address">
-        <Input type="text" @click.native.stop="" v-model="editAddress" v-if="editIndex === index"/>
-        <span v-else>{{ row.address }}</span>
-      </template>
-
-      <template slot-scope="{ row, index }" slot="action">
-        <div v-if="editIndex === index">
-          <Button @click="handleSave(index)">保存</Button>
-          &nbsp;<Button @click="editIndex = -1">取消</Button>
-        </div>
-        <div v-else>
-          <Button @click.stop="handleEdit(row, index)">操作</Button>
-        </div>
-      </template>
-    </Table>
-    <div style="height: 15px;"></div>
+    <!--    <Button @click="getData">请求</Button>-->
+    <!--    <div style="height: 15px"></div>-->
     <Form :label-width="70" @submit.native.prevent="">
       <Row>
         <Col :xs="24" :sm="4" :md="6">
@@ -111,8 +77,6 @@
 <script>
 import { ajax, json_msg } from '@/util'
 import Qs from 'qs'
-import expandRow from './table-expand.vue'
-import { on, off } from '@/libs/tools'
 
 export default {
   data () {
@@ -146,153 +110,11 @@ export default {
       }, {
         title: 'IP地址',
         key: 'ip'
-
       }, {
         title: '操作时间',
         key: 'create_date'
-
       }],
-      logs: [],
-      columns10: [
-        {
-          type: 'expand',
-          width: 50,
-          render: (h, params) => {
-            return h(expandRow, {
-              props: {
-                row: params.row
-              }
-            })
-          }
-        },
-        {
-          title: 'Name',
-          key: 'name',
-          width: 100,
-          resizable: true
-        },
-        {
-          title: 'Age',
-          key: 'age',
-          width: 200,
-          resizable: true
-        },
-        {
-          title: 'Address',
-          key: 'address'
-        }
-      ],
-      data9: [
-        {
-          name: 'John Brown',
-          age: 18,
-          address: 'New York No. 1 Lake Park',
-          job: 'Data engineer',
-          interest: 'badminton',
-          birthday: '1991-05-14',
-          book: 'Steve Jobs',
-          movie: 'The Prestige',
-          music: 'I Cry',
-          children: [
-            {
-              name: 'John Brown',
-              age: 18,
-              address: 'New York No. 1 Lake Park',
-              job: 'Data engineer',
-              interest: 'badminton',
-              birthday: '1991-05-14',
-              book: 'Steve Jobs',
-              movie: 'The Prestige',
-              music: 'I Cry'
-            },
-            {
-              name: 'John Brown',
-              age: 18,
-              address: 'New York No. 1 Lake Park',
-              job: 'Data engineer',
-              interest: 'badminton',
-              birthday: '1991-05-14',
-              book: 'Steve Jobs',
-              movie: 'The Prestige',
-              music: 'I Cry'
-            }
-          ]
-        },
-        {
-          name: 'Jim Green',
-          age: 25,
-          address: 'London No. 1 Lake Park',
-          job: 'Data Scientist',
-          interest: 'volleyball',
-          birthday: '1989-03-18',
-          book: 'My Struggle',
-          movie: 'Roman Holiday',
-          music: 'My Heart Will Go On'
-        },
-        {
-          name: 'Joe Black',
-          age: 30,
-          address: 'Sydney No. 1 Lake Park',
-          job: 'Data Product Manager',
-          interest: 'tennis',
-          birthday: '1992-01-31',
-          book: 'Win',
-          movie: 'Jobs',
-          music: 'Don’t Cry'
-        },
-        {
-          name: 'Jon Snow',
-          age: 26,
-          address: 'Ottawa No. 2 Lake Park',
-          job: 'Data Analyst',
-          interest: 'snooker',
-          birthday: '1988-7-25',
-          book: 'A Dream in Red Mansions',
-          movie: 'A Chinese Ghost Story',
-          music: 'actor'
-        }
-      ],
-      columns: [
-        {
-          title: '姓名',
-          slot: 'name'
-        },
-        {
-          title: '年龄',
-          slot: 'age'
-        },
-        {
-          title: '出生日期',
-          slot: 'birthday'
-        },
-        {
-          title: '地址',
-          slot: 'address'
-        },
-        {
-          title: '操作',
-          slot: 'action'
-        }
-      ],
-      data: [
-        {
-          name: '王小明',
-          age: 18,
-          birthday: '919526400000',
-          address: '北京市朝阳区芍药居'
-        },
-        {
-          name: '张小刚',
-          age: 25,
-          birthday: '696096000000',
-          address: '北京市海淀区西二旗'
-        }
-      ],
-      editIndex: -1, // 当前聚焦的输入框的行数
-      editName: '', // 第一列输入框，当然聚焦的输入框的输入内容，与 data 分离避免重构的闪烁
-      editAge: '', // 第二列输入框
-      editBirthday: '', // 第三列输入框
-      editAddress: '' // 第四列输入框
+      logs: []
     }
   },
   components: {},
@@ -300,10 +122,12 @@ export default {
     search: function () {
       this.curr_page = 1
       for (let o in this.searchData) {
-        if (this.searchData[o].trim()) {
-          this.param[o] = this.searchData[o].trim()
-        } else {
-          delete this.param[o]
+        if (this.searchData.hasOwnProperty(o)) {
+          if (this.searchData[o].trim()) {
+            this.param[o] = this.searchData[o].trim()
+          } else {
+            delete this.param[o]
+          }
         }
       }
       this.getData()
@@ -315,48 +139,13 @@ export default {
         param: JSON.stringify(this.param)
       }), true,
       (data) => {
-        // console.log(data)
         this.logs = data.data
         this.count = Number(data.count)
       },
       () => {
       })
     },
-    login: function () {
-      const data = Qs.stringify({ psw: '123456', username: 'zhengw' })
-      ajax('/Index-loginCheck', data, true,
-        (data) => {
-
-        },
-        () => {
-        })
-    },
-    handleEdit (row, index) {
-      this.editName = row.name
-      this.editAge = row.age
-      this.editAddress = row.address
-      this.editBirthday = row.birthday
-      this.editIndex = index
-    },
-    handleSave (index) {
-      this.data[index].name = this.editName
-      this.data[index].age = this.editAge
-      this.data[index].birthday = this.editBirthday
-      this.data[index].address = this.editAddress
-      this.editIndex = -1
-    },
-    getBirthday (birthday) {
-      const date = new Date(parseInt(birthday))
-      const year = date.getFullYear()
-      const month = date.getMonth() + 1
-      const day = date.getDate()
-      return `${year}-${month}-${day}`
-    },
-    docClick: function () {
-      this.editIndex = -1
-    },
     pageChange: function (val) {
-      console.log(val)
       this.curr_page = val
       this.getData()
     },
@@ -366,7 +155,6 @@ export default {
       } else {
         this.jsonData = json_msg(row.out_param.replace(/[\r\n]/g, ''))
       }
-
       let idx = row._index
       if (idx > this.logs.length - 5 && idx > 5) {
         e.target.parentNode.querySelector('.json-con').className = 'json-con json-con-bottom'
@@ -380,12 +168,7 @@ export default {
     }
   },
   mounted () {
-    // this.getData()
     this.getData()
-    on(document, 'click', this.docClick)
-  },
-  beforeDestroy () {
-    off(document, 'click', this.docClick)
   }
 }
 </script>
@@ -394,9 +177,11 @@ export default {
   .ivu-form-item {
     margin-bottom: 10px;
   }
-.ivu-table-cell{
-  overflow: initial;
-}
+
+  .ivu-table-cell {
+    overflow: initial;
+  }
+
   .param-msg {
     height: 40px;
     overflow: auto;
@@ -428,7 +213,7 @@ export default {
 
   .param-con {
     position: relative;
-    padding: 10px 0;
+    padding: 8px 0;
   }
 
   .json-con {
